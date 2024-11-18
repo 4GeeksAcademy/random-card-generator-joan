9a3d5b88db88720c9a7eb1fb793eb15668caa0b2
Joan <79165234+Joangb85@users.noreply.github.com>
2024-11-18 20:24:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let symbols = ["♦", "♥", "♠", "♣"];
+let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "Q", "K"];
+
+let upper_symbol = document.querySelector(".row1");
+let number = document.querySelector(".row2");
+let lower_symbol = document.querySelector(".row3");
+let card = document.querySelector(".card");
+
+function generate_random_card() {
+  let chosen_suit = symbols[Math.floor(Math.random() * symbols.length)];
+  let chosen_number = numbers[Math.floor(Math.random() * numbers.length)];
+  upper_symbol.innerHTML = chosen_suit;
+  lower_symbol.innerHTML = chosen_suit;
+  number.innerHTML = chosen_number;
+
+  if (chosen_suit == "♦" || chosen_suit == "♥") {
+    upper_symbol.style.color = "red";
+    lower_symbol.style.color = "red";
+  } else {
+    upper_symbol.style.color = "black";
+    lower_symbol.style.color = "black";
+  }
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generate_random_card();
 };
+
+setInterval(function() {
+  generate_random_card();
+}, 10000);
+
+function changeCard(event) {
+  const clickedElement = event.target;
+  if (clickedElement) {
+    return generate_random_card();
+  }
+}
+
+card.addEventListener("click", changeCard);
+
+function changeWidth(event) {
+  const inputElement = event.target;
+  card.style.width = inputElement.value;
+}
+
+cardWidth.addEventListener("input", changeWidth);
+
+function changeHeight(event) {
+  const inputElement = event.target;
+  card.style.height = inputElement.value;
+}
+
+cardHeight.addEventListener("input", changeHeight);
